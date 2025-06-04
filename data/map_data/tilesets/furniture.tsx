99950c8231a7695c266d2e79f8c96f513e5f5833
Gabriel Schönmann <schonmann.gabriel@gmail.com>
2025-06-04 10:24:40 +0200

    Added classroom map - v1

---
+++ data/map_data/tilesets/furniture.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="furniture" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="source/school_furniture.png" width="768" height="768"/>
+</tileset>
